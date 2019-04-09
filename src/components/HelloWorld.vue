70f68c2c574f28311f9ed8999e18b3d53799f2ee
FirYuen <firyuan@gmail.com>
2019-04-10 01:40:57 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,166 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <el-cascader :options="options" v-model="result" :show-all-levels="false" popper-class="aaaa"></el-cascader>
+    <el-button @click="showResult">show Result</el-button>
+    <el-dialog :title="currentResult.title" :visible.sync="dialogVisible" width="80%">
+      <span>{{currentResult.text}}</span>
+      <span slot="footer" class="dialog-footer">
+     
+        <el-button type="primary" @click="dialogVisible = false">Gotcha</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
+
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      result: [],
+      dialogVisible: false,
+      currentResult: {},
+      results: {
+        result1: {
+          title: "FOLLOW-UP TASK",
+          text:
+            "Complete handover steps and send a follow-up task to the appropriate queue."
+        },
+
+        result2: {
+          title: "COLD HANDOVER",
+          text:
+            "Complete handover steps and send a follow-up task to the appropriate queue."
+        },
+        result3: {
+          title: "COLD HANDOVER",
+          text:
+            "Keep the case and continue working with your customer on your next shift"
+        },
+
+        result4: {
+          title: "WARM HANDOVER",
+          text:
+            "Complete handover steps and send a follow-up task to the appropriate queue."
+        }
+      },
+      options: [
+        {
+          value: "node0",
+          label: "Why is handover needed",
+          children: [
+            {
+              value: "node1",
+              label: "Contact needed outside of case owner hours",
+              children: [
+                {
+                  value: "result1",
+                  label:
+                    "Quick follow up action outside of case owner working hours",
+                  children: [
+                    {
+                      value: "result1",
+                      label: "FOLLOW-UP TASK"
+                    }
+                  ]
+                },
+                {
+                  value: "result2",
+                  label:
+                    "Cx has requested to work outside the work hours of the owning Engineer",
+                  children: [
+                    {
+                      value: "result2",
+                      label: "COLD HANDOVER"
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              value: "node2",
+              label: "Case owner is approaching end day",
+              children: [
+                {
+                  value: "node3",
+                  label: "Is customer avaliavle to continue?",
+                  children: [
+                    {
+                      value: "no",
+                      label: "No",
+                      children: [
+                        {
+                          value: "node4",
+                          label: "Cx would like to continue at annother time",
+                          children: [
+                            {
+                              value: "result2",
+                              label:
+                                "Callback will be outside of case owner work hours",
+                              children: [
+                                {
+                                  value: "result2",
+                                  label: "COLD HANDOVER"
+                                }
+                              ]
+                            },
+                            {
+                              value: "result3",
+                              label:
+                                "Callback is agreed during case owner avaliability",
+                              children: [
+                                {
+                                  value: "result3",
+                                  label: "DO NOT HANDOVER"
+                                }
+                              ]
+                            }
+                          ]
+                        }
+                      ]
+                    },
+                    {
+                      value: "yes",
+                      label: "Yes",
+                      children: [
+                        {
+                          value: "result4",
+                          label: "Cx requests continue until resolution",
+                          children: [
+                            {
+                              value: "result4",
+                              label: "WARM HADNOVER"
+                            }
+                          ]
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  },
+  methods: {
+    showResult: function() {
+      let lastResult = this.result[this.result.length - 1];
+      this.currentResult = this.results[lastResult];
+      this.dialogVisible = true;
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.el-cascader {
+  width: 600px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.aaaa {
+  height: 400px;
+  border: 1px solid red;
 }
 </style>
